e14b634f00de16a2cfd632447aa8e2efd211d458
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2023-01-20 17:34:58 -0300

    feat: Crear codigo de JavaScript para realizar las acciones de abrir, cerrar y editar texto en los campos de nombre y acerca de mi

---
+++ :scripts/index.js
@@ -0,0 +1,32 @@
+let btnEdit = document.querySelector(".profile__info-edit-btn");
+let closeBtnPopup = document.querySelector(".popup__container-close-popup");
+let formPopup = document.querySelector(".popup_opened");
+
+let inputName = document.querySelector("#name");
+let inputAboutMe = document.querySelector("#about-me");
+let profileName = document.querySelector(".profile__info-name");
+let profileAboutMe = document.querySelector(".profile__info-about");
+let btnSave = document.querySelector(".popup__container-save-btn");
+
+
+function openPopup(){
+formPopup.classList.toggle("popup_opened");
+} 
+
+btnEdit.addEventListener ("click", openPopup);
+closeBtnPopup.addEventListener ("click", openPopup);
+
+function editProfile(){
+profileName.textContent = inputName.value;
+profileAboutMe.textContent = inputAboutMe.value;
+}
+
+function evtSaveInfo(evt) {
+    evt.preventDefault();
+    editProfile();
+}
+
+btnSave.addEventListener("click", evtSaveInfo);
+btnSave.addEventListener("click", openPopup);
+
+
