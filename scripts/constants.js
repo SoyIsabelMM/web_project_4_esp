dcb2f3a0107e4c9d21144c25b31b60e2a6a20717
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2023-02-14 14:40:12 -0300

    refactor(script js): Organizar el código de JS, agregar un archivo aparte para las constantes, agregar una función para almacenar todos los addEventListener y formatear el código

---
+++ scripts/constants.js
@@ -0,0 +1,76 @@
+/**
+ * Constante que almacena todas las imágenes que aparecenal inicio de nuestro proyecto
+ */
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    src: "./images/valle-yosemite.jpeg",
+  },
+  {
+    name: "Lago Louise",
+    src: "./images/Lago-Louise.jpeg",
+  },
+  {
+    name: "Montañas Calvas",
+    src: "./images/montanas-calvas.jpeg",
+  },
+  {
+    name: "Latemar",
+    src: "./images/latemar.jpeg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    src: "./images/parque-vanoise.jpeg",
+  },
+  {
+    name: "Lago di Braies",
+    src: "./images/lago-dibraies.jpeg",
+  },
+];
+
+/**
+ * Constantes para traer los elementos del modal de imágenes
+ */
+const modalExpandedImage = document.querySelector(".modal");
+const modalExpandedImageCloseBtn = modalExpandedImage.querySelector(
+  ".modal__container-close"
+);
+
+/**Constantes para traer los elementos
+ * para el popup de editar perfil */
+const btnEditInfoProfile = document.querySelector(".profile__info-edit-btn");
+const closeBtnPopup = document.querySelector(".popup__container-close-popup");
+const popupFormeOpened = document.querySelector(".popup_opened");
+
+const inputName = document.querySelector("#name");
+const inputAboutMe = document.querySelector("#about-me");
+const profileName = document.querySelector(".profile__info-name");
+const profileAboutMe = document.querySelector(".profile__info-about");
+const btnSaveProfileInfo = document.querySelector(".popup__container-save-btn");
+
+const form = document.querySelector(".popup__container-form");
+
+/**
+ * Constantes que traen los elementos para el popup
+ * para agregar nuevas imágenes al card-container
+ */
+const btnOpenFormAddImage = document.querySelector(".profile__add-btn");
+const addPictureForm = document.querySelector("#add-picture-form");
+const addPictureFormClose = document.querySelector(
+  "#add-picture-form .popup__container-close-popup"
+);
+
+/**
+ * Con estas constantes estamos clonando el template para nuestras nuevas
+ * card y luego almacenarlas en el bloque elements
+ */
+const elementsSectionCard = document.querySelector(".elements");
+const cardTemplete = document.querySelector("#card-template").content;
+
+/**
+ * Con esta constante traemos los elementos para el botón
+ *  de guardado que se usa en el FORM de add-picture-form
+ */
+const createNewImageBtn = document.querySelector(
+  "#add-picture-form .popup__container-save-btn"
+);
