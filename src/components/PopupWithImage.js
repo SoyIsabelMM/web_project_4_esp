d17db38e7ed675dce81801ee2e1554b605831cc8
Isa <isa@MacBook-Air-de-Daniel.local>
2023-06-27 13:22:16 -0400

    fix: se corrigio la clase PopupWithImage para que apareciera el contenido de alt en caso de que la página no renderice

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,15 +3,17 @@ import Popup from "./Popup.js";
 export default class PopupWhitImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+
+    this._modalImage = document.querySelector(".modal");
+    this._containerImage = this._modalImage.querySelector(".modal__image-card");
+    this._containerTitle = this._modalImage.querySelector(".modal__title-text");
   }
 
   open(src, name) {
-    this.containerImage = document.querySelector(".modal__image-card").src =
-      src;
+    this._containerImage.src = src;
+    this._containerImage.alt = name;
 
-    this.ContainerTitle = document.querySelector(
-      ".modal__title-text"
-    ).textContent = name;
+    this._containerTitle.textContent = name;
 
     this._selector.classList.remove("open");
     document.removeEventListener("keydown", this._handleEscClose);
